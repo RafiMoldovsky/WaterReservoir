2d2395c579b60aab02034d3af66bb159fb05b981
RafiMoldovsky <moldysrm@gmail.com>
2023-04-13 04:46:03 -0700

    Refractored Homepage

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -4,60 +4,93 @@ import mapboxgl from 'mapbox-gl';
 import MyMap from './components/myMap';
 
 const HomePage = () => {
-    const [selectedState, setSelectedState] = useState('');
-    const [siteNos, setSiteNos] = useState([]);
-    const [stationNms, setStationNms] = useState([]);
-    const [selectedSiteNo, setSelectedSiteNo] = useState("");
-    const [selectedStationNm, setSelectedStationNm] = useState("");
-    const [showMap, setShowMap] = useState(false);
-    const handleToggleMap = () => {
-        setShowMap(!showMap);
-      };
-      const handleMapClick = (data) => {
-        const { site_no, station_nm, state_code } = data;
-        setSelectedSiteNo(site_no);
-        setSelectedStationNm(station_nm);
-        setShowMap(false);
-      };
-    const handleStateChange = async event => {
-      const state = event.target.value;
-      setSelectedState(state);
+  const [selectedState, setSelectedState] = useState('');
+  const [siteNos, setSiteNos] = useState([]);
+  const [stationNms, setStationNms] = useState([]);
+  const [selectedSiteNo, setSelectedSiteNo] = useState("");
+  const [selectedStationNm, setSelectedStationNm] = useState("");
+  const [showMap, setShowMap] = useState(false);
+  const [searchMethod, setSearchMethod] = useState("site_no");
+
+  const handleToggleMap = () => {
+    setShowMap(!showMap);
+  };
+
+  const handleMapClick = (data) => {
+    const { site_no, station_nm, state_code } = data;
+    setSelectedSiteNo(site_no);
+    setSelectedStationNm(station_nm);
+    setShowMap(false);
+  };
+
+  const handleStateChange = async event => {
+    const state = event.target.value;
+    setSelectedState(state);
+    setSiteNos([]);
+    setStationNms([]);
+    setSiteNos(["querying"]);
+    setStationNms(["querying"]);
+    if (state) {
+      const data = await getReservoirsAndNamesByState(state);
+      const siteNos = data.flatMap(([site_no]) => site_no);
+      const stationNms = data.flatMap(([, station_nm]) => station_nm);
+      setSiteNos(siteNos);
+      setStationNms(stationNms);
+    } else {
       setSiteNos([]);
       setStationNms([]);
-      setSiteNos(["querying"]);
-      setStationNms(["querying"]);
-      if (state) {
-        const data = await getReservoirsAndNamesByState(state);
-        const siteNos = data.flatMap(([site_no]) => site_no);
-        const stationNms = data.flatMap(([, station_nm]) => station_nm);
-        setSiteNos(siteNos);
-        setStationNms(stationNms);
-      } else {
-        setSiteNos([]);
-        setStationNms([]);
-      }
-      
-    };
+    }
+  };
 
-  
+  const handleSiteChange = (event) => {
+    const selectedOption = event.target.value;
+    const { site_no, station_nm } = JSON.parse(selectedOption);
+    setSelectedSiteNo(site_no);
+    setSelectedStationNm(station_nm);
+  };
 
-    const handleSiteChange = (event) => {
-        const selectedOption = event.target.value;
-        const { site_no, station_nm } = JSON.parse(selectedOption);
-        setSelectedSiteNo(site_no);
-        setSelectedStationNm(station_nm);
-      };
+  const handleSearchMethodChange = (event) => {
+    const method = event.target.value;
+    setSearchMethod(method);
+    setSelectedState("");
+    setSelectedSiteNo("");
+    setSelectedStationNm("");
+    setSiteNos([]);
+    setStationNms([]);
+  };
 
   return (
     <div>
-      <h2>Select a State:</h2>
-      <select value={selectedState} onChange={handleStateChange}>
-        <option value="">--Select a State--</option>
-        <option value="ca">California</option>
-        <option value="nm">New Mexico</option>
-        <option value="az">Arizona</option>
+      <h2>Select a Search Method:</h2>
+      <select value={searchMethod} onChange={handleSearchMethodChange}>
+        <option value="map">Map</option>
+        <option value="state">State</option>
       </select>
-      <h2>Site Numbers and Station Names:</h2>
+      {searchMethod === "map" && (
+        <>
+          <button onClick={handleToggleMap}>
+            {showMap ? "Hide Map" : "Show Map"}
+        </button>
+        {showMap && <MyMap onMapClick={handleMapClick} />}
+        {selectedSiteNo && (
+            <div>
+            <h2>Selected Reservoir:</h2>
+            <p>Site No: {selectedSiteNo}</p>
+            <p>Station Name: {selectedStationNm}</p>
+            </div>
+        )}
+        </>
+      )}
+      {searchMethod === "state" && (
+        <>
+          <h2>Select a State:</h2>
+          <select value={selectedState} onChange={handleStateChange}>
+            <option value="">--Select a State--</option>
+            <option value="ca">California</option>
+            <option value="nm">New Mexico</option>
+            <option value="az">Arizona</option>
+          </select>
+          {selectedState && <h2>Site Numbers and Station Names:</h2>}
       {selectedState && (
         <select value={JSON.stringify({ site_no: selectedSiteNo, station_nm: selectedStationNm })} onChange={handleSiteChange}>
           <option value="">--Select an option--</option>
@@ -68,22 +101,11 @@ const HomePage = () => {
             ))}
         </select>
         )}
-        <br></br>
-        <br></br>
-        <button onClick={handleToggleMap}>
-        {showMap ? "Hide Map" : "Show Map"}
-        </button>
-      {showMap && <MyMap onMapClick={handleMapClick} />}
-      {selectedSiteNo && (
-        <div>
-          <h2>Selected Reservoir:</h2>
-          <p>Site No: {selectedSiteNo}</p>
-          <p>Station Name: {selectedStationNm}</p>
-        </div>
+        </>
       )}
-    </div>
+    <br></br>
     
-
+</div>
   );
 };
 
